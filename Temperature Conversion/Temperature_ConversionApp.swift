4ce50db1ee5ba818975fc60be8b145d31707dec1
romajc84 <joeyroman84@gmail.com>
2023-06-25 07:40:01 -0400

    Initial Commit

---
+++ Temperature Conversion/Temperature_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConversionApp.swift
+//  Temperature Conversion
+//
+//  Created by Joseph Roman on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
