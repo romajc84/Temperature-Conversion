f6655464a62e34d691617d58fe6f6fb150a22ee5
romajc84 <joeyroman84@gmail.com>
2023-06-25 08:46:48 -0400

    Needs updating

--- Temperature Conversion/ContentView.swift
+++ Temperature Conversion/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var temperatureAmountInput = 0.0
+    @State private var temperatureUnitInput = "Celsius"
+    @State private var temperatureUnitOutput = "Fahrenheit"
+    
+    @State private var temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    var temperatureAmountOutput: Double {
+        var output = 0.0
+        
+        if temperatureUnitInput == "Celsius" {
+            if temperatureUnitOutput == "Fahrenheit" {
+                output = (temperatureAmountInput * 9 / 5) + 32
+            } else if temperatureUnitOutput == "Kelvin" {
+                output = temperatureAmountInput + 273.15
+            }
+        } else if temperatureUnitInput == "Fahrenheit" {
+            if temperatureUnitOutput == "Celsius" {
+                output = (temperatureAmountInput - 32) * 5 / 9
+            } else if temperatureUnitOutput == "Kelvin" {
+                output = (temperatureAmountInput - 32) * 5 / 9 + 273.15
+            }
+        } else if temperatureUnitInput == "Kelvin" {
+            if temperatureUnitOutput == "Celsius" {
+                output = temperatureAmountInput - 273.15
+            } else if temperatureUnitOutput == "Fahrenheit" {
+                output = (temperatureAmountInput - 273.15) * 9 / 5 + 32
+            }
         }
-        .padding()
+        
+        return output
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Input")) {
+                    TextField("Amount", value: $temperatureAmountInput, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Unit", selection: $temperatureUnitInput) {
+                        ForEach(temperatureUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section(header: Text("Output")) {
+                    Text("\(temperatureAmountOutput, specifier: "%.2f")")
+                    
+                    Picker("Unit", selection: $temperatureUnitOutput) {
+                        ForEach(temperatureUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            }
+            .navigationTitle("Temperature Conversion")
+        }
     }
 }
 
